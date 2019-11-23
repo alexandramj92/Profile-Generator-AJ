f3b6ec586dc8650995f2cee297052f64b97352c0
Alexandra Luna <alexandra.michelle.j@gmail.com>
2019-11-22 17:58:57 -0800

    worked on generated pdf layout

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const PDFDocument = require('pdfkit');
+const doc = new PDFDocument ({compress:false});
 
-const doc = new PDFDocument();
+let fullName="";
+let imgURL="";
+let company="";
+let blog ="";
+let location="";
+let email="";
+let githubLink="";
+let publicRepos="";
+let followers="";
+let following="";
+let githubStars="";
+let cardColor="";
+
+
+
 
 inquirer.prompt([
+// prompts user for their github username and their favorite color
     
     {
         type:"input",
@@ -19,49 +35,128 @@ inquirer.prompt([
         name: "favoriteColor"
     }
 
-]).then(function({username}){
+]).then(function({username, favoriteColor}){
 
     const queryUrl = `https://api.github.com/users/${username}`;
     const starredUrl = `https://api.github.com/users/${username}/starred`;
+    cardColor = favoriteColor;
 
     axios.get(queryUrl).then(function(res){
 
         //retrieving information that will go in the PDF
+   
 
-        const fullName = res.data.name;
+        fullName = res.data.name;
         console.log(fullName);
 
-        const company =  res.data.company;
+        imgURL = res.data.avatar_url;
+        console.log(imgURL);
+
+        company =  res.data.company;
         console.log(company);
 
-        const blog = res.data.blog;
+        blog = res.data.blog;
         console.log(blog);
 
-        const location = res.data.location; 
+        location = res.data.location; 
         console.log(location);
 
-        const email = res.data.email;
+        email = res.data.email;
         console.log(email);
 
-        const githubLink = res.data.html_url;
+        githubLink = res.data.html_url;
         console.log(githubLink);
 
-        const publicRepos = res.data.public_repos;
+        publicRepos = res.data.public_repos;
         console.log(publicRepos);
 
-        const followers = res.data.followers;
+        followers = res.data.followers;
         console.log(followers);
 
-        const following = res.data.following;
+        following = res.data.following;
         console.log(following);
     });
 
     axios.get(starredUrl).then(function(res){
         // retrieving the length of starred repositories to get the number of github stars
-        const githubStars = res.data.length;
+        githubStars = res.data.length;
         console.log(githubStars);
+        generatePDF();
+
     });
-    
+})
 
 
-});
+function generatePDF(){
+
+   //generate PDF
+   doc.pipe(fs.createWriteStream('file.pdf')); // write to PDF
+  //  doc.pipe(res); // HTTP response
+   // add stuff to PDF here using methods described below...
+   
+   doc
+   .rect(20, 50, 570, 250)
+   .fill(`${cardColor}`)
+
+   doc
+   .rect(80, 400, 200, 100)
+   .fill(`${cardColor}`)
+   .fontSize(20)
+   .fillColor('white')
+   .text('Public Repositories', 95, 440)
+   .text(`${publicRepos}`, 160)
+
+   doc
+   .rect(340, 400, 200, 100)
+   .fill(`${cardColor}`)
+   .fontSize(20)
+   .fillColor('white')
+   .text('Followers', 400, 440)
+   .text(`${followers}`, 420)
+
+   doc
+   .rect(80, 540, 200, 100)
+   .fill(`${cardColor}`)
+   .fontSize(20)
+   .fillColor('white')
+   .text('Github Stars', 120, 580)
+   .text(`${githubStars}`, 160)
+   
+   
+  doc
+   .rect(340, 540, 200, 100)
+   .fill(`${cardColor}`)
+   .fontSize(20)
+   .fillColor('white')
+   .text('Following', 400, 580)
+   .text(`${following}`, 420)
+
+   doc.fillColor('white').fontSize(30).text('Hi!', 300, 150)
+   doc.fillColor('white').fontSize(30).text(`My name is ${fullName}!`, 120)
+   doc.fillColor('white').fontSize(20).text(`Currently @ ${company}`, 190)
+   doc.fillColor('white').fontSize(20).text(`${location}`, 170, 270)
+
+   doc.fillColor('white').fontSize(20).text('github', 290, 270)
+   const widthGl = doc.widthOfString('github');
+   const heightGl = doc.currentLineHeight();
+   doc.link(290, 270, widthGl, heightGl, `${githubLink}`);
+
+
+   doc.fillColor('white').fontSize(20).text("blog", 420, 270)
+  const widthB = doc.widthOfString('blog');
+  const heightB = doc.currentLineHeight();
+  doc.link(420, 270, widthB, heightB, `${blog}`);
+
+  
+
+   doc.moveDown()
+   doc.fillColor('black').fontSize(25)
+   .text('I build things and teach people to code', 100)
+
+   
+   // finalize the PDF and end the stream
+   doc.end();
+   
+
+
+};
